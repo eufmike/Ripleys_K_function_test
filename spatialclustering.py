00fe5d9b63cd36a46f09fffaa79e7e041a962ab0
Michael Shih <sc2.shih@gmail.com>
2018-07-09 12:09:43 -0500

    model done

--- spatialclustering.py
+++ spatialclustering.py
@@ -26,3 +26,25 @@ rate, Dx = 0.2, 20
 P = PoissonPP(rate, Dx).T
 plt.scatter(P[0], P[1], edgecolor = 'b', facecolor='none', alpha =0.5)
 plt.show()
+
+#%%
+# Poisson test
+import scipy.stats
+
+rt = 0.2
+Dx = 20
+Dy = 20
+N = 80
+print(N)
+# N = scipy.stats.poisson(rt*Dx*Dy).rvs()
+# print(N)
+print(((N, 1)))
+x = scipy.stats.uniform.rvs(loc = 0, scale = Dx, size = ((N, 1))) 
+# 0, Dx means range, ((N, 1)) means dimentional structure
+
+y = scipy.stats.uniform.rvs(0, Dy, ((N, 1)))
+
+print(len(x))
+print('x = {}'.format(x))
+# print(f"x = {x}")
+# print(y)
