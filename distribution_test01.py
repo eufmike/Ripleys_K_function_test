b9221bdb79a80581829ef77fc97561a1d06ffc0d
Michael Shih <sc2.shih@gmail.com>
2018-07-12 17:34:32 -0500

    modify simulator

--- distribution_test01.py
+++ distribution_test01.py
@@ -8,12 +8,11 @@ import matplotlib.pyplot as plt
 dir = '/Users/michaelshih/Documents/code/personal_project/Ripleys_K_function_test/'
 os.chdir(dir)
 
+#%%
 import imp
 import spatialstatWUCCI.distribution_simulator as sswdistsim
 imp.reload(sswdistsim)
 
-
-#%%
 # ----------------------------------------------------------------------------
 # make plot
 import seaborn as sns
@@ -21,34 +20,52 @@ import seaborn as sns
 # reset style 
 sns.reset_orig()
 
+# set seed for simulation
+seed = 1219
+
 # define the size of the canvas
 plt.figure(figsize= (10, 10))
 plotsize_x = 20.0
 plotsize_y = 20.0
 
+# plot 1
 plot_1 = plt.subplot(221)
 # create simulation
-rate = 1/400
+rate = 1/200
 Dx = 20
-seed = 1000
-P = sswdistsim.PoissonPP(rate, Dx).T
-np.random.seed(seed=1223)
-
-N = scipy.stats.poisson(rate*Dx*Dx).rvs()
-print(N)
+P = sswdistsim.PoissonPP(rt = rate, Dx = Dx, seed = seed).T
 
 plot_1.scatter(P[0], P[1], edgecolor = 'r', marker = '.', facecolor='none', alpha =0.7)
 plot_1.set_xlim((0, 20))
 plot_1.set_ylim((0, 20))
 
-
+# plot 2
 plot_2 = plt.subplot(222)
-rate = 0.2
+# create simulation
+rate = 1/200
 Dx = 20
-P = sswdistsim.PoissonPP(rate, Dx).T
+sigma = 0.3
+mu = 15
+P = sswdistsim.ThomasPP(rt = rate, Dx = Dx, sigma = sigma, mu = mu, seed = seed)
+
+Dy = Dx
+N = scipy.stats.poisson(rate*Dx*Dy).rvs(random_state=seed)
+print('Dx test = {}'.format(Dx))
+print('Dy test = {}'.format(Dy))
+print('N test = {}'.format(N))
+
 plt.scatter(P[0], P[1], edgecolor = 'b', facecolor='none', alpha =0.7)
 plot_2.set_xlim((0, 20))
 plot_2.set_ylim((0, 20))
 
-
 plt.show()
+
+#%%
+seed = 1219
+mu = 15
+sigma = 0.3
+i = [0, 0]
+pdf = scipy.stats.norm(loc = i, scale = (sigma, sigma))
+N = scipy.stats.poisson(mu).rvs(random_state=seed)
+XYP = list(pdf.rvs(2, random_state = seed))
+print(XYP)
--- spatialstatWUCCI/distribution_simulator.py
+++ spatialstatWUCCI/distribution_simulator.py
@@ -17,42 +17,64 @@ def PoissonPP(rt, Dx, Dy = None, seed = None):
     given the rate 'rt' and the dimensions, 'Dx', 'Dy'.
     Returns a <2xN> NumPy array.
     '''
-    if seed != None: 
-        np.random.seed(seed=seed)
     
     if Dy == None:
         Dy = Dx
-
-    N = scipy.stats.poisson(rt*Dx*Dy).rvs()
-    x = scipy.stats.uniform.rvs(loc = 0, scale = Dx, size = ((N, 1)))
-    y = scipy.stats.uniform.rvs(loc = 0, scale = Dx, size = ((N, 1)))
+    if seed == None: 
+        N = scipy.stats.poisson(rt*Dx*Dy).rvs()
+        x = scipy.stats.uniform.rvs(loc = 0, scale = Dx, size = ((N, 1)))
+        y = scipy.stats.uniform.rvs(loc = 0, scale = Dx, size = ((N, 1)))
+    else:
+        N = scipy.stats.poisson(rt*Dx*Dy).rvs(random_state=seed)
+        x = scipy.stats.uniform.rvs(loc = 0, scale = Dx, size = ((N, 1)), random_state=seed)
+        y = scipy.stats.uniform.rvs(loc = 0, scale = Dx, size = ((N, 1)), random_state=seed)
+        print('Dx = {}'.format(Dx))
+        print('Dy = {}'.format(Dy))
+        print('N = {}'.format(N))
+    
     P = np.hstack((x, y))
     return(P)
 
-def ThomasPP(kappa, sigma, mu, Dx):
+def ThomasPP(rt, Dx, sigma, mu, seed = None):
     '''
     each forming a Poisson (mu) numbered cluster of points,
     having an isotropic Gaussian distribution with variance 'sigma'
     '''
-    
+
     # Create a set of parent points form a Poisson(kappa)
     # distribution on the square region [0, Dx] * [0, Dx]
-    parents = PoissonPP(kappa, Dx)
+    
+    if seed == None:
+        parents = PoissonPP(rt, Dx)
+    else:    
+        parents = PoissonPP(rt, Dx, seed = seed)
     # M is the number of parents
     M = parents.shape[0]
     # an empty list for the Thomas process points
-    TP  = []
+    x = []
+    y = []
     # for each parent point
     for i in range(M):
         # determine a number of children accorfing to a Poisson(mu) distribution    
-        N = scipy.stats.poisson(mu).rvs()
-    # for each child point
+        pdf = scipy.stats.norm(loc = parents[i, :2], scale = (sigma, sigma))
+        if seed == None:
+            N = scipy.stats.poisson(mu).rvs()
+            XYP = list(pdf.rvs(N * 2))
+        else:
+            N = scipy.stats.poisson(mu).rvs(random_state=seed)
+            print('N poisson= {}'.format(N))
+            XYP = list(pdf.rvs(N*2, random_state = seed))
+            print('with seed XYP = {} '.format(XYP))
+            x = x.append(XYP[:N])
+            y = y.appemd(XYP[N:])
+        '''
         for j in range(N):
             # place a point centered on the location of the parent according 
             # to an isotropuc Gaussuan distribution with sigma variance
                 pdf = scipy.stats.norm(loc = parents[i, :2], scale = (sigma, sigma))
                 # add the child point to the list TP
-                TP.append(list(pdf.rvs(2)))
+                TP.append(list(pdf.rvs(2, random_state=seed)))
+        '''
     x, y = zip(*TP)
     pts = [x, y]
     return pts
