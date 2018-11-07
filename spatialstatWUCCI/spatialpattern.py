adff67ec08f4c15b065a8d7b54ef4b49f0e912a1
Michael Shih <sc2.shih@gmail.com>
2018-11-07 12:29:25 -0600

    print in branch

--- spatialstatWUCCI/spatialpattern.py
+++ spatialstatWUCCI/spatialpattern.py
@@ -14,6 +14,7 @@ import random
 import spatialstatWUCCI.distribution_simulator as sswdistsim
 
 print("test branch _push")
+print("test branch _push 2")
 # spest: general spatial pattern analysis
 def spest(xyarray_ref, xyarray_all, rstart, rend, density, rsize = None, rstep = 0.1, function = 'Hest', downsize = True, downsizesize = 3000):
     '''
